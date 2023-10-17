6a4d570cc49042f9de56e17cac27eafb32c0e1cb
SMAILHAMMADI <hammadi_sma@hotmail.fr>
2023-10-17 08:51:54 -0700

    a

--- main.h
+++ main.h
@@ -1,1 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+#define BUFF_SIZE 1024
+
+
+/****************** FUNCTIONS ******************/
+
 int _printf(const char *format, ...);
+
+#endif /* MAIN_H */
